1ed5008341869ff3b5d931a36dd5e857b3324425
Nicole D'Anton <nicoledanton7@gmail.com>
2025-08-16 12:40:04 -0500

    added window alert for showing unloading haulers when you click on a dock

--- scripts/docksList.js
+++ scripts/docksList.js
@@ -1,4 +1,4 @@
-import { getDocks } from "./database.js"
+import { getDocks, getHaulingShips } from "./database.js"
 
 
 export const DockList = () => {
@@ -6,7 +6,9 @@ export const DockList = () => {
     let docksHTML = "<ul>"
 
     for (const dock of docks) {
-        docksHTML += `<li data-dockid="${dock.id}"> 
+        docksHTML += `<li data-dockid="${dock.id}"
+                          data-type="dock"
+                          data-location="${dock.location}"> 
         ${dock.location} can hold ${dock.volume} of cargo
         </li>`
     }
@@ -15,3 +17,31 @@ export const DockList = () => {
 
     return docksHTML
 }
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.dataset.type === "dock"){
+            const dockId = itemClicked.dataset.dockid
+            let dockUnload = []
+            const haulers = getHaulingShips()
+            for (const hauler of haulers){
+                if (parseInt(dockId)=== hauler.dockId){
+                    dockUnload.push(hauler.name)
+                }
+            } 
+            if (dockUnload.length > 0){
+                const haulerList = dockUnload.join(",")
+                window.alert(`The ${itemClicked.dataset.location} dock is currently unloading ${haulerList}`)
+                 }
+            else {
+                window.alert (`The ${itemClicked.dataset.location} dock is currently unloading nothing`)
+                }
+        /*
+            Your job is to design an algorithm and implement it
+        */
+        }
+    }
+)
