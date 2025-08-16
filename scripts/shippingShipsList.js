4c6bfbcf4703b59b6197c9b4d4f199884a6ff11b
Nicole D'Anton <nicoledanton7@gmail.com>
2025-08-16 12:20:55 -0500

    added a window alert for clicking on a shipping ship showing the hauler name carrying it

--- scripts/shippingShipsList.js
+++ scripts/shippingShipsList.js
@@ -1,4 +1,4 @@
-import { getShippingShips } from "./database.js"
+import { getHaulingShips, getShippingShips } from "./database.js"
 
 
 export const shippingShipsList = () => {
@@ -7,7 +7,9 @@ export const shippingShipsList = () => {
 
     for (const ship of shippingShips) {
         shippingShipsHTML += `<li data-shipid="${ship.id}"
-                                    data-haulerforeignkey="${ship.haulerId}"> 
+                                    data-haulerforeignkey="${ship.haulerId}"
+                                    data-type="shipping ship"
+                                    data-name= "${ship.name}"> 
                                 ${ship.name}
                                  </li>`
     }
@@ -16,3 +18,32 @@ export const shippingShipsList = () => {
 
     return shippingShipsHTML
 }
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+
+        if (itemClicked.dataset.type === "shipping ship") {    // Was a shipping ship list item clicked?
+
+            // Get the haulerId value of the shipping ship clicked
+            const haulerForeignKey = itemClicked.dataset.haulerforeignkey //this is a string!!!
+
+            // Define a default object for the found hauler
+            let haulingShip = { name: "Incorrect" }
+
+            // Iterate the array of hauler objects
+            const haulers = getHaulingShips()
+            for (const hauler of haulers) {
+                // Does the haulerId foreign key match the id of the current hauler?
+                if (parseInt(haulerForeignKey) === hauler.id) {
+                    haulingShip = `${hauler.name}` // Reassign the value of `haulingShip` to the current hauler
+                }
+            }
+            
+            // Show an alert to the user with this format.. Palais Royal is being hauled by Seawise Giant
+            window.alert(`${itemClicked.dataset.name} is being hauled by ${haulingShip}`)
+        }
+    }
+)
